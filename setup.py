d64f414210f4e27ec0e8fbea771c49b777bb6792
AmalieDue <amalieduejensen@hotmail.com>
2022-09-09 14:09:06 +0200

    Make initial setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='D1-client-python',
+    version='0.0.0',
+    author='Amalie Due Jensen',
+    install_requires=[
+        "pytest",
+        "pylint",
+    ],
+)
