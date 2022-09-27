6398638500e840c6a58e0d57ba74054da8420461
Philip Vejre <philip@cyber-crypt.com>
2022-09-27 10:43:53 +0200

    Ignore E1101 from pylint. The missing members are generated dynamically by internal protobuf code.

--- d1_storage/storage.py
+++ d1_storage/storage.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=E1101
+
 """This module contains the StorageClient class."""
 
 from d1_generic import base
