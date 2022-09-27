64e56d37d47a3cbcebf00fe81926dfe88c5f23c5
AmalieDue <amalieduejensen@hotmail.com>
2022-09-27 10:13:37 +0200

    Update setup.py in d1_generic package

--- d1_generic/setup.py
+++ d1_generic/setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
-
 setup(
     name='d1_generic',
     version='0.0.0',
     description='A Python D1 Generic client',
     license="Apache License, Version 2.0",
-    long_description=long_description,
     packages=['d1_generic'],
 )
